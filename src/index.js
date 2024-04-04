5b6687a45d6847e07b5d9096d7d568f0e2d55156
eunbin <dmsqlsdl54@gmail.com>
2024-04-04 19:12:04 +0900

    가위바위보 결과 함수 추가

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const WINS = {
+  rock: "scissor",
+  scissor: "paper",
+  paper: "rock",
+};
+
+const getResult = (left, right) => {
+  if (WINS[left] === right) return "승리";
+  else if (left === WINS[right]) return "패배";
+  else return "무승부";
+};
+
+const handleClick = () => {
+  console.log("가위바위보!");
+};
+
+const me = "rock";
+const other = "scissor";
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <h1>가위바위보</h1>
+    <h2>{getResult(me, other)}</h2>
+    <button onClick={handleClick}>가위</button>
+    <button onClick={handleClick}>바위</button>
+    <button onClick={handleClick}>보</button>
+  </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
